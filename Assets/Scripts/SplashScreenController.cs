74529be0ded1b5e57d887efcd28ab0fa6b1bd5f4
kamyab78 <amirhosseinghassemi78@yahoo.com>
2025-05-24 19:18:01 +0300

    Add splash screen

---
+++ Assets/Scripts/SplashScreenController.cs
@@ -0,0 +1,33 @@
+using System.Collections;      
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenController : MonoBehaviour
+{
+    // public CanvasGroup logo;
+    public Animator circleAnimator;
+    public string nextSceneName = "2048";
+    void Start()
+    {
+        StartCoroutine(PlaySplash());
+    }
+
+    IEnumerator PlaySplash()
+    {
+        yield return new WaitForSeconds(2f);  // Wait before circle expands
+        circleAnimator.Play("CircleExpand.anim");
+
+        // Optional: fade logo out while circle expands
+        float duration = 1.5f;
+        float time = 0f;
+        while (time < duration)
+        {
+            // logo.alpha = 1f - (time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        // logo.alpha = 0;
+        SceneManager.LoadScene(nextSceneName);
+
+    }
+}
